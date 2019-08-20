3092385d7bd6068c9f2b8d3453a33cea43295cf5
linna <cello1412@gmail.com>
2019-08-20 16:59:03 +1000

    matching and if let

--- pattern_matching/src/main.rs
+++ pattern_matching/src/main.rs
@@ -27,6 +27,40 @@ fn value_in_cents(coin: Coin) -> u8 {
     }
 }
 
+/* 
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    which can be written as:
+*/
+fn if_let() {
+    // The syntax if let takes a pattern and an expression separated by an equal sign. It works the same way as a match, where the expression is given to the match and the pattern is its first arm.
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+}
+
+/*
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+
+    is the same as:
+*/
+fn if_let_else() {
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
